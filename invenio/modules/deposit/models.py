6f9937ab5a5507f9c8b8bb4676fe096daebedc20
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-12-21 12:49:25 +0100

    deposit: workflow delete fix
    * Fixes issue where deleting a single unpublished deposit object
      could result in other objects being deleted as well.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/modules/deposit/models.py
+++ invenio/modules/deposit/models.py
@@ -1085,15 +1085,14 @@ class Deposition(object):
         for f in self.files:
             f.delete()
 
-        if self.workflow_object.id_workflow != '':
-            if self.workflow_object.id_workflow:
-                Workflow.delete(uuid=self.workflow_object.id_workflow)
+        if self.workflow_object.id_workflow:
+            Workflow.delete(uuid=self.workflow_object.id_workflow)
 
             BibWorkflowObject.query.filter_by(
                 id_workflow=self.workflow_object.id_workflow
             ).delete()
         else:
-            db.session.remove(self.workflow_object)
+            db.session.delete(self.workflow_object)
         db.session.commit()
 
     #
